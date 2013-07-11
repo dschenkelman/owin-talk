a3f98c3bba2fb20c7dd169be06150d76be6623d6
dschenkelman <damian.schenkelman@gmail.com>
2013-07-11 00:38:32 -0300

    Added deck, source. Still to add instructions to run demo

---
+++ src/Owin.IpFilter/Extensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Owin.IpFilter
+{
+    using System;
+    using System.Net;
+
+    public static class Extensions
+    {
+        public static IAppBuilder UseIpFiltering(this IAppBuilder appBuilder, Func<IPAddress, bool> rejectRequest)
+        {
+            appBuilder.Use(typeof(IpFilterMiddleware), rejectRequest);
+            return appBuilder;
+        }
+    }
+}
---
+++ src/Owin.Sample/Startup.cs
@@ -0,0 +1,37 @@
+﻿namespace Owin.Sample
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.IO;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using Owin.IpFilter;
+    using AppFunc = System.Func<System.Collections.Generic.IDictionary<string, object>, System.Threading.Tasks.Task>;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            appBuilder.UseIpFiltering(
+                remoteAddress =>
+                    {
+                        var bytes = remoteAddress.GetAddressBytes();
+                        return bytes[0] != 192 && bytes[0] != 172 && bytes[0] != 10 && bytes[0] != 127 && bytes[0] != 0;
+                    });
+            
+            appBuilder.Use(new Func<AppFunc, AppFunc>(ignoredNextMiddleware => this.HelloWorld));
+        }
+
+        public Task HelloWorld(IDictionary<string, object> env)
+        {
+            var responseBytes = Encoding.UTF8.GetBytes("Hello world from OWIN");
+            var responseStream = (Stream)env["owin.ResponseBody"];
+            var responseHeaders = (IDictionary<string, string[]>)env["owin.ResponseHeaders"];
+            responseHeaders["Content-Length"] = new[] { responseBytes.Length.ToString(CultureInfo.InvariantCulture) };
+            responseHeaders["Content-Type"] = new[] { "text/plain" };
+            return responseStream.WriteAsync(responseBytes, 0, responseBytes.Length);
+        }
+    }
+}
